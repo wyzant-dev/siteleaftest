a90c4e760e90e40d82a30e718aa2a12f3fcfd07d
Vah <bvah@wyzant.com>
2016-11-10 16:31:23 -0600

    adding scripts and styles back

---
+++ blog/scripts/main.js
@@ -0,0 +1,25 @@
+(function() {
+
+    function init() {
+    	handleNavClick();
+    }
+
+    function handleNavClick() {
+    	var navButton = document.getElementById("menu");
+    	var nav = document.getElementById("nav");
+    	navButton.addEventListener("click", function() {
+    		var navState = nav.getAttribute("data-expanded");
+
+    		if (navState == "false") {
+    			nav.setAttribute("data-expanded", "true");
+    		}
+    		else {
+    			nav.setAttribute("data-expanded", "false");
+    		}
+    	});
+    }
+
+    init();
+
+
+})();
